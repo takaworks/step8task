3ff4a42fddfb3a308d7938450c8d00c0b3d5cdd8
takaworks <takaworks525@gmail.com>
2022-12-08 22:59:55 +0900

    テスト

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <!-- <a href="{{ url('/home') }}">Home</a> -->
+
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
